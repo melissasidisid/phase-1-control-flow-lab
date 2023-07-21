7390b9fff5eebf323e8807b1173332a218b28868
melissa sidisaid <milyssasidisaid@gmail.com>
2023-07-21 03:42:09 +0200

    done it is done

--- index.js
+++ index.js
@@ -2,35 +2,47 @@
 let answer;
   
   function scuberGreetingForFeet(feet){
-
-
-    if (feet <= 400) {
-      answer = "This one is on me!";
+if (feet <= 400) {
+  return  "This one is on me!";
+}
+ else if (feet > 400 && feet <= 2000) {
+     return  "That will be twenty bucks.";
     }
-    else if (feet > 400 && feet <= 2000) {
-      answer = "That will be twenty bucks.";
+     else if (feet>2000 && feet <= 2500){
+      return'I will gladly take your thirty bucks.';
     }
     else if (feet>2000 && feet <= 2500){
-      answer = 'I will gladly take your thirty bucks.';
-    }else if (feet >= 2500){
-      answer = 'No can do.';
+      return 'I will gladly take your thirty bucks.';
     }
+    else if (feet >= 2500){
+     return 'No can do.';
+    
 
-  return answer;
+    }
+    
+
+    
 }
 
 function ternaryCheckCity(city){
-return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  if (city=='NYC'){
+    return 'Ok, sounds good.'
+  }else{
+         return 'No go.'
+  }
+
 }
 
 function switchOnCharmFromTip(tip){
-
-switch (tip){
+switch(tip){
   case 'generous' :
     return 'Thank you so much.';
+    break;
   case 'not as generous' :
     return 'Thank you.';
+    break
   default :
     return 'Bye.';
 }
+
 }
